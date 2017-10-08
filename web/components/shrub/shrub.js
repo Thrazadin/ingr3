837ee32f37185250236225a80f6bab6db862111b
Marshal Dickey <mdickey14@gmail.com>
2017-10-08 00:59:44 -0400

    update

--- web/components/shrub/shrub.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('ingr3', ["chart.js"])
-  .controller("Shrub", function ($scope) {
-      $scope.labels = ['Orange', 'Shrub', 'Carrots', 'Mint'];
-      $scope.series = ['Health Value'];
-      $scope.data = [
-        [1, 0.3, 0.8, -0.6]
-      ];
-  });
