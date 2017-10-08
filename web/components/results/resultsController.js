a3d9de753133aa7df92befd8b7291c13434bcf5d
Marshal Dickey <mdickey14@gmail.com>
2017-10-08 01:56:18 -0400

    ez descriptions

--- web/components/results/resultsController.js
+++ web/components/results/resultsController.js
@@ -1,8 +1,19 @@
 angular
     .module('ingr3', ["chart.js"])
     .controller("BarCtrl", function ($scope) {
-        $scope.labels = ['Orange', 'Shrub', 'Carrots', 'Mint', 'asdfasd', 'asdfasdfa', 'asdfasdfa'];
         $scope.series = ['Health Value'];
         $scope.data = [ [ 1, 0.3, 0.8, -1, -0.2, 0.4, 0.7 ] ];
-        $scope.descriptions = [];
+        $scope.labels = ['Orange', 'Shrub', 'Carrots', 'Mint', 'asdfasd', 'asdfasdfa', 'asdfasdfa'];
+        $scope.descriptions = ['lkasjdlfkja', 'lkasjdlfkja', 'lkasjdlfkja', 'lkasjdlfkja', 'lkasjdlfkja', 'lkasjdlfkja', 'lkasjdlfkja'];
+        $scope.barChartOptions = {
+          showToolTips: false,
+          tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+          tooltips: {
+              callbacks: {
+                  beforeLabel: function(tooltipItem, data) {
+                      return $scope.descriptions[tooltipItem.index];
+                  },
+              }
+          }
+        };
     });
